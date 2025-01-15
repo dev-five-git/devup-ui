fe37e1004869cd64cf46cb71868d74f53602158d
owjs3901 <owjs3901@naver.com>
2025-01-16 02:54:28 +0900

    Fix rev

--- libs/extractor/src/prop_modify_utils.rs
+++ libs/extractor/src/prop_modify_utils.rs
@@ -19,30 +19,29 @@ pub fn modify_prop_object<'a>(
     let mut class_name_prop = None;
     let mut style_prop = None;
 
-    for idx in 0..props.len() {
-        if let ObjectPropertyKind::ObjectProperty(attr) = &props[idx] {
+    for idx in (0..props.len()).rev() {
+        if let ObjectPropertyKind::ObjectProperty(attr) = props.remove(idx) {
             if let PropertyKey::StaticIdentifier(ident) = &attr.key {
                 if ident.name == "className" {
-                    if let ObjectPropertyKind::ObjectProperty(attr) = props.remove(idx) {
-                        class_name_prop = Some(attr);
-                    }
+                    class_name_prop = Some(attr);
+                    continue;
                 } else if ident.name == "style" {
-                    if let ObjectPropertyKind::ObjectProperty(attr) = props.remove(idx) {
-                        style_prop = Some(attr);
-                    }
+                    style_prop = Some(attr);
+                    continue;
                 }
             }
+            props.insert(idx, ObjectPropertyKind::ObjectProperty(attr));
         }
     }
 
     // should modify class name prop
     if let Some(ex) = gen_class_names(ast_builder, &styles) {
         if let Some(pr) = if let Some(class_name_prop) = class_name_prop {
-            let res = merge_expression_for_class_name(
+            merge_expression_for_class_name(
                 ast_builder,
                 vec![class_name_prop.value.clone_in(ast_builder.allocator), ex],
-            );
-            res.map(|res| {
+            )
+            .map(|res| {
                 ast_builder.alloc_object_property(
                     SPAN,
                     PropertyKind::Init,
@@ -75,41 +74,37 @@ pub fn modify_prop_object<'a>(
 
     // should modify style prop
     if let Some(mut ex) = gen_styles(ast_builder, &styles) {
-        if let Some(style_prop) = style_prop {
-            props.push(ObjectPropertyKind::ObjectProperty(
-                ast_builder.alloc_object_property(
-                    SPAN,
-                    PropertyKind::Init,
-                    PropertyKey::StaticIdentifier(ast_builder.alloc_identifier_name(SPAN, "style")),
-                    if ex.properties.is_empty() {
-                        Expression::ObjectExpression(ast_builder.alloc(ex))
-                    } else {
-                        ex.properties.push(ObjectPropertyKind::SpreadProperty(
-                            ast_builder.alloc_spread_element(
-                                SPAN,
-                                style_prop.value.clone_in(ast_builder.allocator),
-                            ),
-                        ));
-                        Expression::ObjectExpression(ast_builder.alloc(ex))
-                    },
-                    false,
-                    false,
-                    false,
-                ),
-            ));
+        props.push(if let Some(style_prop) = style_prop {
+            ObjectPropertyKind::ObjectProperty(ast_builder.alloc_object_property(
+                SPAN,
+                PropertyKind::Init,
+                PropertyKey::StaticIdentifier(ast_builder.alloc_identifier_name(SPAN, "style")),
+                if ex.properties.is_empty() {
+                    Expression::ObjectExpression(ast_builder.alloc(ex))
+                } else {
+                    ex.properties.push(ObjectPropertyKind::SpreadProperty(
+                        ast_builder.alloc_spread_element(
+                            SPAN,
+                            style_prop.value.clone_in(ast_builder.allocator),
+                        ),
+                    ));
+                    Expression::ObjectExpression(ast_builder.alloc(ex))
+                },
+                false,
+                false,
+                false,
+            ))
         } else {
-            props.push(ObjectPropertyKind::ObjectProperty(
-                ast_builder.alloc_object_property(
-                    SPAN,
-                    PropertyKind::Init,
-                    PropertyKey::StaticIdentifier(ast_builder.alloc_identifier_name(SPAN, "style")),
-                    Expression::ObjectExpression(ast_builder.alloc(ex)),
-                    false,
-                    false,
-                    false,
-                ),
-            ));
-        };
+            ObjectPropertyKind::ObjectProperty(ast_builder.alloc_object_property(
+                SPAN,
+                PropertyKind::Init,
+                PropertyKey::StaticIdentifier(ast_builder.alloc_identifier_name(SPAN, "style")),
+                Expression::ObjectExpression(ast_builder.alloc(ex)),
+                false,
+                false,
+                false,
+            ))
+        });
     } else if let Some(style_prop) = style_prop {
         // re add class name prop if not modified
         props.push(ObjectPropertyKind::ObjectProperty(style_prop))
@@ -125,18 +120,17 @@ pub fn modify_props<'a>(
     let mut style_prop = None;
 
     for idx in (0..props.len()).rev() {
-        if let Attribute(attr) = &props[idx] {
+        if let Attribute(attr) = props.remove(idx) {
             if let Identifier(ident) = &attr.name {
                 if ident.name == "className" {
-                    if let Attribute(attr) = props.remove(idx) {
-                        class_name_prop = Some(attr);
-                    }
+                    class_name_prop = Some(attr);
+                    continue;
                 } else if ident.name == "style" {
-                    if let Attribute(attr) = props.remove(idx) {
-                        style_prop = Some(attr);
-                    }
+                    style_prop = Some(attr);
+                    continue;
                 }
             }
+            props.insert(idx, Attribute(attr));
         }
     }
 
