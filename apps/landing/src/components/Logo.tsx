a4a852db8661e7056c541079418f4f39bd035db0
owjs3901 <owjs3901@naver.com>
2025-01-27 22:56:33 +0900

    Fix logo

--- apps/landing/src/components/Logo.tsx
+++ apps/landing/src/components/Logo.tsx
@@ -1,11 +1,12 @@
 import { Center, Flex, Image } from '@devup-ui/react'
 
+import { URL_PREFIX } from '../constants'
 import { LogoText } from './LogoText'
 
 export function Logo() {
   return (
     <Flex alignItems="center" gap={[2, null, 4]} h={[7, '42px']}>
-      <Image h="100%" src="/logo.svg" />
+      <Image h="100%" src={URL_PREFIX + '/logo.svg'} />
       <Center h={[5, null, '42px']}>
         <LogoText />
       </Center>
