a17c7b87d36ae430380f73194a92c0545cb3ffc8
owjs3901 <owjs3901@naver.com>
2025-01-27 22:22:38 +0900

    Add color

--- packages/react/src/types/props/svg.ts
+++ packages/react/src/types/props/svg.ts
@@ -1,10 +1,11 @@
 import type { Property } from 'csstype'
 
 import type { ResponsiveValue } from '../responsive-value'
+import type { DevupThemeColors } from '../theme'
 
 export interface DevupUiSvgProps {
-  fill?: ResponsiveValue<Property.Fill>
-  stroke?: ResponsiveValue<Property.Stroke>
+  fill?: ResponsiveValue<Property.Fill | keyof DevupThemeColors>
+  stroke?: ResponsiveValue<Property.Stroke | keyof DevupThemeColors>
   strokeWidth?: ResponsiveValue<Property.StrokeWidth>
   strokeOpacity?: ResponsiveValue<Property.StrokeOpacity>
 }
