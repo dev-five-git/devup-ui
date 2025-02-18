7535643ad688567f53707a1ac7a6b64b6184ad8e
owjs3901 <owjs3901@naver.com>
2025-02-18 19:31:02 +0900

    Fix default generic type of VStack

--- packages/react/src/components/VStack.tsx
+++ packages/react/src/components/VStack.tsx
@@ -1,7 +1,7 @@
 import type { DevupProps } from '../types/props'
 import type { Merge } from '../types/utils'
 
-export function VStack<T extends React.ElementType = 'span'>(
+export function VStack<T extends React.ElementType = 'div'>(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   props: Merge<React.ComponentProps<T>, DevupProps<T>>,
 ): React.ReactElement {
