20a2faa4a3763af97618622ed32d479c7363cc8f
owjs3901 <owjs3901@naver.com>
2025-04-08 22:21:35 +0900

    Fix vite plugin, Apply css optimize

--- apps/vite/src/App.tsx
+++ apps/vite/src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Text } from '@devup-ui/react'
+import { Box, Text } from '@devup-ui/react'
 import { Lib } from 'vite-lib-example'
 
 export default function App() {
@@ -18,22 +18,6 @@ export default function App() {
       <Text color="#777"></Text>
       <Text color="#777"></Text>
       <Text typography="header">typo</Text>
-      <NoContent />
     </div>
-  )
-}
-export function NoContent(props: { text?: string; background?: boolean }) {
-  const { text, background } = props
-  return (
-    <Center
-      bg={background ? 'var(--white-bg)' : undefined}
-      h="100%"
-      minH="80px"
-      w="100%"
-    >
-      <Text color="var(--gray-color)" fontSize="13px" textAlign="center">
-        {text ?? '표시할 정보가 없습니다.'}
-      </Text>
-    </Center>
   )
 }
