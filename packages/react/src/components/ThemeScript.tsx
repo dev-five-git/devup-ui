c029a8de334b4447cc7fbfc5b75bc866c1ceafa2
owjs3901 <owjs3901@naver.com>
2025-04-09 15:29:18 +0900

    Fix ThemeScript init issue

--- packages/react/src/components/ThemeScript.tsx
+++ packages/react/src/components/ThemeScript.tsx
@@ -12,7 +12,7 @@ export function ThemeScript({ auto = true, theme }: ThemeScriptProps) {
     <script
       dangerouslySetInnerHTML={{
         __html: theme
-          ? `(function (){document.documentElement.setAttribute('data-theme',${theme});}())`
+          ? `(function (){document.documentElement.setAttribute('data-theme','${theme}');}())`
           : `(function (){document.documentElement.setAttribute('data-theme',localStorage.getItem('__DF_THEME_SELECTED__')||(${String(auto)}&&window.matchMedia('(prefers-color-scheme:dark)').matches?'dark':'${process.env.DEVUP_UI_DEFAULT_THEME ?? 'default'}'));})()`,
       }}
     />
