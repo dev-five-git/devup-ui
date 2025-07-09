8a210529b94d6da47147a1bbc42983abeb35e22f
Hanbin Cho <forestream@devfive.kr>
2025-07-09 09:43:01 +0900

    Fix layout

--- apps/landing/src/app/(detail)/components/layout.tsx
+++ apps/landing/src/app/(detail)/components/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
-      <Flex maxW="1920px" minH="calc(100vh - 500px)" mx="auto" w="100%">
+      <Flex maxW="1920px" minH="calc(100vh - 500px)" mx="auto">
         <Box display={['none', null, 'initial']} p="20px 16px" w="220px">
           <Box pos="sticky" top={['70px', null, '90px']}>
             <LeftMenu />
@@ -16,9 +16,10 @@ export default function Layout({
         </Box>
         <Box
           className="markdown-body"
-          overflow="hidden"
-          px={['16px', null, '60px']}
-          py={['24px', null, '40px']}
+          flex={1}
+          px={['16px', '30px', '60px']}
+          py={['24px', '40px']}
+          w="100%"
         >
           {children}
         </Box>
--- apps/landing/src/app/(detail)/team/layout.tsx
+++ apps/landing/src/app/(detail)/team/layout.tsx
@@ -7,7 +7,13 @@ export default function TeamLayout({
 }>) {
   return (
     <>
-      <Box maxW="1014px" minH="calc(100vh - 500px)" mx="auto" p="40px 60px">
+      <Box
+        maxW="1014px"
+        minH="calc(100vh - 500px)"
+        mx="auto"
+        px={['16px', null, '60px']}
+        py={['20px', null, '40px']}
+      >
         {children}
       </Box>
     </>
