97a1679716b6a4e83fb7bcde35f920eab342673e
owjs3901 <owjs3901@gmail.com>
2025-07-16 19:44:41 +0900

    Add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,46 @@
+import { DevupUI } from '@devup-ui/vite-plugin'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      include: ['packages/*/src/**'],
+      exclude: [
+        'packages/*/src/types',
+        'packages/*/src/**/__tests__',
+        '**/*.stories.{ts,tsx}',
+      ],
+      cleanOnRerun: true,
+      reporter: ['text', 'json', 'html'],
+    },
+    projects: [
+      {
+        test: {
+          name: 'node',
+          include: ['packages/*/src/**/__tests__/**/*.test.{ts,tsx}'],
+          exclude: ['packages/*/src/**/__tests__/**/*.browser.test.{ts,tsx}'],
+          globals: true,
+          environment: 'node',
+        },
+      },
+
+      {
+        test: {
+          name: 'happy-dom',
+          include: ['packages/*/src/**/__tests__/**/*.browser.test.{ts,tsx}'],
+          environment: 'happy-dom',
+          globals: true,
+          css: true,
+          setupFiles: ['@testing-library/jest-dom/vitest'],
+        },
+        plugins: [
+          DevupUI({
+            debug: true,
+          }),
+        ],
+      },
+    ],
+    cache: false,
+  },
+})
