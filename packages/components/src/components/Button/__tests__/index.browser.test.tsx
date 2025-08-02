a374a98968ac0fb32364cf656bd6f9048971c7b5
owjs3901 <owjs3901@gmail.com>
2025-08-03 01:50:32 +0900

    Update snapshot

--- packages/components/src/components/Button/__tests__/index.browser.test.tsx
+++ packages/components/src/components/Button/__tests__/index.browser.test.tsx
@@ -89,7 +89,7 @@ describe('Button', () => {
     )
     expect(container).toMatchSnapshot()
     const button = container.querySelector('button>div>div')
-    expect(button).toHaveClass('textOverflow-0-ellipsis--255')
+    expect(button).toHaveClass('text-overflow-0-ellipsis--255')
   })
 
   it('should have font size 15px when size is md and variant is primary', () => {
@@ -99,7 +99,7 @@ describe('Button', () => {
       </Button>,
     )
     expect(container).toMatchSnapshot()
-    expect(container.querySelector('button')).toHaveClass('fontSize-0-15px--1')
+    expect(container.querySelector('button')).toHaveClass('font-size-0-15px--1')
   })
 
   it('should have font size 15px when size is sm and variant is primary', () => {
