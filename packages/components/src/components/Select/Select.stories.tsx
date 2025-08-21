4d39b863fc788f35c981d0de33cbf00ed55939c8
belltalion <xogus4026@gmail.com>
2025-08-21 14:45:11 +0900

    Fix select componenet

--- packages/components/src/components/Select/Select.stories.tsx
+++ packages/components/src/components/Select/Select.stories.tsx
@@ -54,39 +54,33 @@ function DefaultComponent(
   props: Omit<ComponentProps<typeof Select>, 'children'>,
 ) {
   return (
-    <Flex justifyContent="flex-end">
-      <Select {...props} defaultValue={['Option 1']} onChange={() => {}}>
-        <SelectTrigger>Select2</SelectTrigger>
-        <SelectContainer>
-          <SelectOption disabled value="Option 1">
-            Option 1
-          </SelectOption>
-          <SelectOption value="Option 2">Option 2</SelectOption>
-          <SelectDivider />
-          <SelectOption value="Option 3">Option 3</SelectOption>
-          <SelectOption disabled value="Option 4">
-            Option 4
-          </SelectOption>
-          <Select type="radio">
-            <SelectTrigger asChild>
-              <SelectOption>
-                <Flex
-                  alignItems="center"
-                  justifyContent="space-between"
-                  w="100%"
-                >
-                  Option 5<IconArrow />
-                </Flex>
-              </SelectOption>
-            </SelectTrigger>
-            <SelectContainer>
-              <SelectOption value="Option 6">Option 6</SelectOption>
-              <SelectOption value="Option 7">Option 7</SelectOption>
-            </SelectContainer>
-          </Select>
-        </SelectContainer>
-      </Select>
-    </Flex>
+    <Select {...props} defaultValue={['Option 1']} onChange={() => {}}>
+      <SelectTrigger>Select2</SelectTrigger>
+      <SelectContainer>
+        <SelectOption disabled value="Option 1">
+          Option 1
+        </SelectOption>
+        <SelectOption value="Option 2">Option 2</SelectOption>
+        <SelectDivider />
+        <SelectOption value="Option 3">Option 3</SelectOption>
+        <SelectOption disabled value="Option 4">
+          Option 4
+        </SelectOption>
+        <Select type="radio">
+          <SelectTrigger asChild>
+            <SelectOption>
+              <Flex alignItems="center" justifyContent="space-between" w="100%">
+                Option 5<IconArrow />
+              </Flex>
+            </SelectOption>
+          </SelectTrigger>
+          <SelectContainer>
+            <SelectOption value="Option 6">Option 6</SelectOption>
+            <SelectOption value="Option 7">Option 7</SelectOption>
+          </SelectContainer>
+        </Select>
+      </SelectContainer>
+    </Select>
   )
 }
 
