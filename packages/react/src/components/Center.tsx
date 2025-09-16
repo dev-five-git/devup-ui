f7ee823c898a535785aae52422727400c6c44d13
owjs3901 <owjs3901@gmail.com>
2025-09-17 00:42:44 +0900

    Add as typing

--- packages/react/src/components/Center.tsx
+++ packages/react/src/components/Center.tsx
@@ -1,9 +1,12 @@
-import type { DevupComponentProps } from '../types/props'
+import type {
+  DevupComponentBaseProps,
+  DevupComponentProps,
+} from '../types/props'
 import type { Merge } from '../types/utils'
 
 export function Center<T extends React.ElementType = 'div'>(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  props: Merge<React.ComponentProps<T>, DevupComponentProps<T>>,
+  props: Merge<DevupComponentBaseProps<T>, DevupComponentProps<T>>,
 ): React.ReactElement {
   throw new Error('Cannot run on the runtime')
 }
