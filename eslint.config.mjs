99e1824f1b3ac835365650f8f92f9c54690bb16e
hyu-dev <hyu630115@gmail.com>
2025-09-29 02:09:08 +0900

    Fix eslint rules
    - cleanup eslint rule configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,49 @@
-import tsParser from '@typescript-eslint/parser'
 import { configs } from 'eslint-plugin-devup'
 import jsonc from 'eslint-plugin-jsonc'
 import * as mdx from 'eslint-plugin-mdx'
-import jsonParser from 'jsonc-eslint-parser'
+import globals from 'globals'
 
 export default [
+  // eslint-plugin-devup
   ...configs.recommended,
+  // eslint-plugin-jsonc
+  ...jsonc.configs['flat/recommended-with-json'],
+  ...jsonc.configs['flat/recommended-with-jsonc'],
+  // globals (node, browser, builtin)
   {
-    files: ['**/*.{json,json5,jsonc}'],
-    languageOptions: { parser: jsonParser },
-    plugins: { jsonc },
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.builtin,
+      },
+    },
     rules: {
-      'prettier/prettier': 'off',
-      'eol-last': ['error', 'never'],
-      'no-multiple-empty-lines': ['error', { max: 0, maxEOF: 0 }],
+      // js require import allowed
+      '@typescript-eslint/no-require-imports': 'off',
     },
   },
+  // benchmark no console rules
+  {
+    files: ['benchmark.js'],
+    rules: {
+      'no-console': [
+        'error',
+        {
+          allow: ['info', 'debug', 'warn', 'error', 'profile', 'profileEnd'],
+        },
+      ],
+    },
+  },
+  // create-style-context.mjs no children prop
+  {
+    files: ['**/*.mjs'],
+    rules: {
+      'react/no-children-prop': 'off',
+    },
+  },
+  // md, mdx rules
   {
     ...mdx.flat,
     files: ['**/*.{md,mdx}'],
@@ -23,15 +51,13 @@ export default [
       lintCodeBlocks: true,
     }),
   },
+  // md, mdx code blocks rules
   {
     ...mdx.flatCodeBlocks,
     files: ['**/*.{md,mdx}/*.{js,jsx,ts,tsx}'],
-    languageOptions: { parser: tsParser },
     rules: {
       ...mdx.flatCodeBlocks.rules,
       'react/jsx-no-undef': 'off',
-      semi: ['error', 'never'],
-      quotes: ['error', 'single', { avoidEscape: true }],
       'react/jsx-tag-spacing': ['error', { beforeClosing: 'never' }],
     },
   },
