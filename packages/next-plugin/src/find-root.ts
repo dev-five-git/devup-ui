c60294b8f9aa2dda20c3632a5564993fc838f4c1
owjs3901 <owjs3901@gmail.com>
2025-10-28 19:13:43 +0900

    Fix find root

--- packages/next-plugin/src/find-root.ts
+++ packages/next-plugin/src/find-root.ts
@@ -4,16 +4,13 @@ import { dirname, join } from 'node:path'
 export function findRoot(dir: string): string {
   let root = dir
   let prev = null
-  const collectecd: string[] = []
+  let result: string = process.cwd()
   while (prev === null || root !== prev) {
-    if (existsSync(join(root, 'package.json')) && !collectecd.includes(root)) {
-      collectecd.push(root)
+    if (existsSync(join(root, 'package.json'))) {
+      result = root
     }
     prev = root
     root = dirname(root)
   }
-  if (collectecd.length > 0) {
-    return collectecd.pop()!
-  }
-  return process.cwd()
+  return result
 }
