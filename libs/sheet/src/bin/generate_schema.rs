dc3539f1fc2bdc40a9148ba2354c447bcd703458
owjs3901 <owjs3901@gmail.com>
2025-12-30 21:08:47 +0900

    Rm schema

--- libs/sheet/src/bin/generate_schema.rs
+++
@@ -1,8 +0,0 @@
-use schemars::schema_for;
-use sheet::theme::DevupJson;
-
-fn main() {
-    let schema = schema_for!(DevupJson);
-    let json = serde_json::to_string_pretty(&schema).unwrap();
-    println!("{}", json);
-}
