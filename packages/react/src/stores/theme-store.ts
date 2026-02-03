a0b16e29cc122f937bc47452d37fb291f275e821
owjs3901 <owjs3901@gmail.com>
2026-02-04 00:53:42 +0900

    Fix test

--- packages/react/src/stores/theme-store.ts
+++ packages/react/src/stores/theme-store.ts
@@ -4,12 +4,10 @@ import type { DevupTheme } from '../types/theme'
 type Theme = keyof DevupTheme | null
 type StoreChangeEvent = (newTheme: Theme) => void
 
-const initTheme = null
-
 function createClientThemeStore() {
   const el = document.documentElement
   const subscribers: Set<StoreChangeEvent> = new Set()
-  let theme: Theme = initTheme
+  let theme: Theme = null
   const get = () => theme
   const set = (newTheme: Theme) => {
     theme = newTheme
@@ -45,7 +43,7 @@ export function createServerThemeStore(): ReturnType<
   typeof createClientThemeStore
 > {
   return {
-    get: () => initTheme,
+    get: () => null,
     set: () => {},
     subscribe: () => () => {},
   } as unknown as ReturnType<typeof createClientThemeStore>
