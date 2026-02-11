c11131406b731825d7c58bff1e21d06154dfd177
owjs3901 <owjs3901@gmail.com>
2026-02-12 03:07:44 +0900

    Update action

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 4,
+  workers: process.env.CI ? 2 : 4,
   reporter: 'html',
   timeout: 60_000,
   expect: {
