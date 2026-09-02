da46feceffb17b3e9f27d1a75fab927f7055eafe
devfive <devfive@devfiveui-MacStudio.local>
2026-09-02 09:35:39 +0900

    fix(landing): update header wordmark

--- apps/landing/src/components/Logo.tsx
+++ apps/landing/src/components/Logo.tsx
@@ -4,9 +4,9 @@ import { LogoText } from './LogoText'
 
 export function Logo() {
   return (
-    <Flex alignItems="center" gap={[2, null, 4]} h={[7, '42px']}>
+    <Flex alignItems="center" gap={[2, null, 3]} h={[7, '42px']}>
       <Image h="100%" src="/logo.svg" />
-      <Center h={[5, null, '42px']}>
+      <Center h={['18.67px', null, '28px']}>
         <LogoText />
       </Center>
     </Flex>
